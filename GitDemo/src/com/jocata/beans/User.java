8c449cce90e9e3b1cb4433def15b156f533f27a0
Dev2 <Dev2@gmail.com>
2020-04-10 23:04:23 +0530

    properties added to user.java

--- GitDemo/src/com/jocata/beans/User.java
+++ GitDemo/src/com/jocata/beans/User.java
@@ -1,5 +1,36 @@
 package com.jocata.beans;
 
 public class User {
-
+	
+	private int userId;
+	private String name;
+	private String phoneNo;
+	private String email;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
