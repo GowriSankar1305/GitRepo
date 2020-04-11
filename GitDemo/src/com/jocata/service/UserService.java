6b5163900ed0b609bfada4c8e765a99a85285325
Dev2 <Dev2@gmail.com>
2020-04-11 11:30:36 +0530

    user service class methods are added

--- GitDemo/src/com/jocata/service/UserService.java
+++ GitDemo/src/com/jocata/service/UserService.java
@@ -1,5 +1,27 @@
 package com.jocata.service;
 
-public class UserService {
+import java.util.List;
 
+import com.jocata.beans.User;
+import com.jocata.dao.UserDao;
+
+public class UserService {
+	
+	UserDao dao = new UserDao();
+	
+	public void saveUser(User user)	{
+		dao.saveUser(user);
+	}
+	
+	public void displayAllEmployees()	{
+		List<User> users = dao.getAllUsers();
+		if (users != null && users.size() > 0)	{
+			users.forEach(item -> {
+				System.out.println(item.getName()+" "+item.getPhoneNo()+" "+item.getEmail());
+			});
+		}
+		else	{
+			System.out.println("No users found...");
+		}
+	}
 }
