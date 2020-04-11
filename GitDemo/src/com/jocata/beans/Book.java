fca4466a774f8bedab9419e9c649d51ebdd885e1
Dev2 <Dev2@gmail.com>
2020-04-11 11:49:38 +0530

    book class added by devloper1

---
+++ GitDemo/src/com/jocata/beans/Book.java
@@ -0,0 +1,35 @@
+package com.jocata.beans;
+
+public class Book {
+	
+	private long bookId;
+	private String bookName;
+	private double price;
+	private String authorName;
+	
+	public long getBookId() {
+		return bookId;
+	}
+	public void setBookId(long bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	
+}
