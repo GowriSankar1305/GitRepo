da249ef196c44a0f014a3015fce04451f1f46285
Dev2 <Dev2@gmail.com>
2020-04-11 11:12:36 +0530

    added insertion logic to save user in database

--- GitDemo/src/com/jocata/dao/UserDao.java
+++ GitDemo/src/com/jocata/dao/UserDao.java
@@ -1,11 +1,44 @@
 package com.jocata.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.jocata.beans.User;
 
 public class UserDao {
 	
+	Connection con = null;
+	PreparedStatement ps = null;
+	Statement st = null;
+	
 	public void saveUser(User user)	{
-		System.out.println("saving user...");
-		System.out.println(user);
+		String insertQuery = "insert into user values (?,?,?,?)";
+		try	{
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "root123");
+			ps = con.prepareStatement(insertQuery);
+			ps.setInt(1, user.getUserId());
+			ps.setString(2, user.getName());
+			ps.setString(3, user.getPhoneNo());
+			ps.setString(4, user.getEmail());
+			ps.executeUpdate();
+		}
+		catch(SQLException se)	{
+			se.printStackTrace();
+		}
+		finally {
+			try {
+				if (con != null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (ps != null) ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
